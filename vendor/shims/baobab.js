e0e565ed9cd747152312d9459529f57478a8ffe2
lifeart <lifeart92@gmail.com>
2017-10-26 04:50:43 +0300

    shim update

--- vendor/shims/baobab.js
+++ vendor/shims/baobab.js
@@ -3,10 +3,7 @@
   function vendorModule(trueBaobab) {
     'use strict';
 
-    return {
-      'default': trueBaobab,
-      __esModule: true,
-    };
+    return trueBaobab;
   }
   // export an object with a default property that contains the 'hljs' global.
   define('baobab', ['baobab/baobab'], vendorModule);
